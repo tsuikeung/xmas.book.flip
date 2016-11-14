695e1c71d9da8784570d5b709b9cf0ec977e551a
tsuikeung <tsuiking2004@yahoo.com.hk>
2016-11-15 02:02:55 +0800

    Revert "fix mobile issue"
    This reverts commit d473c5e2837859a0e0d2f6d0179ee5eaed5ca032.

--- js/xmas-book.js
+++ js/xmas-book.js
@@ -89,113 +89,7 @@ $( function() {
 		id = getHashID();
 		gotoPage = function( p ) {
 				pageflip.gotoPage( p, true );
-			}
-		/* MEET PAGEFLIP CUSTOM FUNCTIONS */
-			var animmode = 1, 
-				animbtns = [ "#animateontop", "#animateonvisible" ],
-				loop = false,
-				rot = 0,
-				target,
-				zooming = false,
-				CustomPFEventHandler = {
-					onFlip: function( PN ) 	{ 
-						if( PN==18 ) $("#isflipping").addClass("selected");
-					},
-					onFlipEnd: function( PN ) {
-						if( PN==18 ) $("#isflipping").removeClass("selected");
-					},
-					onTop: function( PN ) {
-						if( PN==21 && animmode==0 ) startLoop();
-						if( PN==18 ) $("#isontop").addClass("selected");
-					},
-					onTopEnd: function( PN ) {
-						if( PN==21 && animmode==0 ) stopLoop();
-						if( PN==18 ) $("#isontop").removeClass("selected");
-					},
-					onLoad: function( PN ) {
-						if( PN==11 ) videoInit();
-						if( PN==18 ) setZoomFlag();
-					},
-					onUnload: function( PN ) {
-						if( PN==21 && animmode==1 ) stopLoop();
-					},
-					onHide: function( PN ) {
-						if( PN==21 && animmode==1 ) stopLoop();
-						if( PN==18 ) $("#isvisible").removeClass("selected");
-					},
-					onShow: function( PN ) {
-						if( PN==21 ) {
-							if( animmode==1 ) startLoop();
-							else setRot();
-							animation( animmode );
-						}
-						if( PN==18 ) $("#isvisible").addClass("selected");
-					},
-					onZoomIn: function( PN ) {
-						zooming = true;
-						setZoomFlag();
-					},
-					onZoomOut: function( PN ) {
-						zooming = false;
-						setZoomFlag();
-					}
-				};
-				
-			gotoPage = function( p ) {
-				pageflip.gotoPage( p, true );
-			}
-			videoPlay = function() {
-				var myVideo = document.getElementById("thevideo");
-				if (myVideo.paused) myVideo.play(); 
-				else myVideo.pause(); 
-			}
-			videoInit = function() {
-				//var myVideo = document.getElementById("thevideo");
-			}
-			selectFeature = function( f ) {
-				$(".features").addClass("off");
-				$("#feature"+f).removeClass("off");
-				$("#featurelist ul li").removeClass("selected");
-				$("#option"+f).addClass("selected");
-			}
-			animation = function( t ) {
-				animmode = t;
-				$(".animationcontrol").removeClass("selected");
-				$(animbtns[t]).addClass("selected");
-			}
-			theLoop = function() {
-				if( loop ) raf2( theLoop );
-				setRot();
-			}
-			setRot = function() {
-				$("#spiral").css( { transform: "rotate("+rot+"deg)" } );
-				rot = ( rot+4 )%360;
-			}
-			startLoop = function() {
-				if( !loop ) {
-					loop = true;
-					theLoop();
-				}
-			}
-			stopLoop = function() {
-				loop = false;
-			}
-			setZoomFlag = function() {
-				if( zooming ) $("#iszooming").addClass("selected");
-				else $("#iszooming").removeClass("selected");
-			}
-			window.raf2 = (function(){
-				return 	window.requestAnimationFrame       ||
-						window.webkitRequestAnimationFrame ||
-						window.mozRequestAnimationFrame    ||
-						function( callback ) {
-						window.setTimeout( callback, 100/6 );
-				};
-			})();
-			
-			/* END MEET PAGEFLIP CUSTOM FUNCTIONS */
-
-				
+			}	
 	/* start the first book automatically! */
 		startPageflip = function( id ) {
 		if( closing || id==loadedID ) return
@@ -211,7 +105,6 @@ $( function() {
 			if( ( defaultID==id && h && h!=id ) || ( defaultID!=id && h!=id ) ) location.hash = id;
 			$pageflip.pageflipInit( bookConfig[id], id );
 			pageflip = $pageflip.pageflip();
-			pageflip.setPFEventCallBack( CustomPFEventHandler );
 		}
 	};
 	
